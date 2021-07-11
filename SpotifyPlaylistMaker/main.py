fdfd3825ea4afd72e4c79d71e4526e4fd5090403
Tszhim <74326452+Tszhim@users.noreply.github.com>
2021-07-11 18:38:37 -0400

    Update main.py

--- SpotifyPlaylistMaker/main.py
+++ SpotifyPlaylistMaker/main.py
@@ -4,13 +4,3 @@ from song_generator import SongGenerator
 song_generator = SongGenerator()
 playlist_creator = PlaylistCreator()
 playlist_creator.generate_playlist(song_generator.playlist_date, song_generator.get_songs())
-
-
-
-
-
-
-
-
-
-
